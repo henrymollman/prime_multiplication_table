7abdf9cbffd6e2aeac2ac6e333304df7e25ec661
henrymollman <hmollman@gmail.com>
2015-08-14 23:17:40 -0700

    Fixed help

--- app/generateTable.js
+++ app/generateTable.js
@@ -2,29 +2,31 @@ var GetPrimes = require('./getPrimes.js');
 var getPrimes = new GetPrimes();
 
 if (process.argv[2] === "-h" || process.argv[2] === "--help") {
-  console.log('\n # prime_multiplication_table
-A node.js script to generate a command-line representation of a multiplication table made up of prime numbers
+  var help = "\nprime_multiplication_table \n" + 
+"\nA node.js script to generate a command-line representation of a multiplication table made up of prime numbers\n" + 
+"\n" + 
+"# Installation on Linux\n" + 
+"type `sudo apt-get install nodejs nodejs-dev npm` to install node if you have not done so. Once node is installed, clone the repo\n" +
+"and install jasmine by typing `npm install` on the command line from the root directory.\n" +
+"\n" +
+"Then, run the script by typing the following command from the root directory:\n" +
+"\n" +
+"`node app/generateTable.js x [option]`\n" +
+"\n" +
+"Where x is the number of prime numbers to generate, or the range from 0- whatever is specified if the '-sieve' option is toggled.\n" +
+"\n" +
+"Use `node app/generateTable.js -h` or `--help` for information about the scripts parameters.\n" +
+"\n" + 
+"# Tests\n" + 
+"Tests are located in the 'spec' subfolder, and can be run by typing\n" +
+"\n" +
+"`jasmine-node spec/`\n" +
+"\n" +
+"from the command line. If any tests fail, information will be provided.\n" +
+"\n" +
+"Thanks, and stay frosty\n"
 
-# Installation on Linux
-type `sudo apt-get install nodejs nodejs-dev npm` to install node if you have not done so. Once node is installed, clone the repo 
-and install jasmine by typing `npm install` on the command line from the root directory.
-
-Then, run the script by typing the following command from the root directory:
-
-`node app/generateTable.js x [option]`
-
-Where x is the number of prime numbers to generate, or the range from 0- whatever is specified if the "-sieve" option is toggled.
-
-Use `node app/generateTable.js -h` or `--help` for information about the scripts parameters.
-
-# Tests
-Tests are located in the "spec" subfolder, and can be run by typing
-
-`jasmine-node spec/`
-
-from the command line. If any tests fail, information will be provided.
-
-Thanks, and stay frosty');
+  console.log(help);
 }
 
 else {
